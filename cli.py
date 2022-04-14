3772bb98088fad8e6e25a59ee143124226c971dd
GregRos <greg.r@reflectiz.com>
2022-04-14 21:10:45 +0300

    torrent_root can be variable

--- cli.py
+++ cli.py
@@ -9,10 +9,10 @@ def get_cli():
         "title",
         "content"
     ], default="all", type=str)
-    classify.add_argument("torrent_root", help="path to the torrent's folder")
+    classify.add_argument("torrent_root", help="path to the torrent's folder", nargs="+")
 
     copy = actions.add_parser("sort", help="sort torrent")
-    copy.add_argument("torrent_root", help="root folder containing the torrent")
+    copy.add_argument("torrent_root", help="root folder containing the torrent", nargs="+")
     copy.add_argument("--dry-run", help="see what happens")
     copy.add_argument("--dest", help="new content root", required=True)
     copy.add_argument("--action", help="sorting action", default="copy", choices=[
